baa504b2a829fbc9472e5d2a68800aea9e53cc64
fabianportillodc <72370904@ucsm.edu.pe>
2021-10-14 10:56:07 -0500

    Experiencia 1

---
+++ AppMVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppMVC1.Startup))]
+namespace AppMVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
